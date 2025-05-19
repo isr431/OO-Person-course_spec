f4cb0f1c4f8798d354aa19b1d809684d8585e231
Israel <100066524+isr431@users.noreply.github.com>
2025-05-19 04:03:43 +0000

    Update

--- main.py
+++ main.py
@@ -20,6 +20,38 @@ class Student(Person):
             subjects[subject] = [self.id]
         
         self.subjects.append(subject)
+    
+    def displaySubjects(self):
+        print("Subjects:")
+
+        for subject in self.subjects:
+            print("-", subject)
+    
+    def run(self):
+        while True:
+            mode = input("Would you like to display all subjects or enrol in a class (display/enrol/exit): ").lower()
+
+            if mode == "display":
+                self.displaySubjects()
+            elif mode == "enrol":
+                subject = input("Enter subject: ").lower()
+                self.enrolClass(subject)
+                print(f"Student successfully enrolled in {subject}.")
+            elif mode == "exit":
+                break
+            else:
+                print("Invalid mode.")
+
+class Parent(Person):
+    def __init__(self, fname, lname, *children: Student):
+        super().__init__(fname, lname)
+        self.children = list(children)
+    
+    def displayChildren(self):
+        print("Children:")
+
+        for child in self.children:
+            print("-", child)
 
 def printStudentList(subject):
     print(f"Students: {', '.join(str(student) for student in subjects[subject])}")
@@ -28,5 +60,9 @@ subjects = {}
 
 israel = Student("Israel", "Irawan", 1234, "Johnson")
 israel.enrolClass("Math")
-print(israel.subjects)
-printStudentList("Math")
+israel.enrolClass("English")
+israel.enrolClass("Software Engineering")
+israel.enrolClass("Legal Studies")
+israel.enrolClass("Enterprise Computing")
+israel.enrolClass("Studies of Religion")
+israel.displaySubjects()
